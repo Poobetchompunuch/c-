9b03196afcd4658b2c74f094545cf3a9bccaa587
Poobetchompunuch <139093944+Poobetchompunuch@users.noreply.github.com>
2023-07-10 10:38:37 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   cout << "Student Code : 6606021620163" << endl;
+    cout << "Student Name : Poobet Chompunuch" << endl;
+	cout << "    Division : Information Technology" << endl;
+	cout << "        Year : 1" << endl;
+	cout << "        Room : RA" << endl;
+	cout << "         Sex : Male" << endl;
+	cout << "         Age : 18" << endl;
+    system("pause");
+	return(0);
+}
